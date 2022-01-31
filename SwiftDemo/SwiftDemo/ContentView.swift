98a7de2485e07271a4f260572f8cf5cf01e87a39
Tzuzul Code <mail@tzuzulcode.com>
2022-01-31 12:02:44 -0600

    Ejemplo de State properties y ejemplos de vistas

--- SwiftDemo/SwiftDemo/ContentView.swift
+++ SwiftDemo/SwiftDemo/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    var colors: [Color] = [.black, .red, .green, .blue]
+    var colorNames = ["Black","Red","Green","Blue"]
+    @State private var rotation:Double = 0
+    @State private var text = "Hola mundo desde Swift"
+    @State private var colorIndex = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text(text)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(rotation))
+                .animation(.easeInOut(duration: 1))
+                .foregroundColor(colors[colorIndex])
+                .padding()
+            Spacer()
+            Divider()
+            Slider(value: $rotation,in: 0 ... 360,step: 0.1)
+                .padding()
+            
+            TextField("Ingresa mensaje de bienvenida...",text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Picker(selection: $colorIndex, label: Text("Color")){
+                ForEach(0..<colorNames.count){ color in
+                    Text(colorNames[color])
+                }
+                /*ForEach(0..<colorNames.count){
+                    Text(colorNames[$0])
+                        .foregroundColor(colors[$0])
+                }*/
+            }.padding()
+        }
     }
 }
 
